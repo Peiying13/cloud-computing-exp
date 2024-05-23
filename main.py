227769ee57b3f986f24a7c96b1f3fc138c92b68a
Peiying13 <peiying.ling@griffithuni.edu.au>
2024-05-23 20:27:44 +1000

    new

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import base64
 app = Flask(__name__)
 
 ### TO EDIT ###
-sql_host = '34.87.225.240' 
-sql_connection_name='ultimate-bit-387412:australia-southeast1:googlecloudsql'
+sql_host = '35.189.20.61' 
+sql_connection_name='pelagic-firefly-423902-d2:australia-southeast1:googlecloudsql'
 sql_port = 3306
 sql_database = 'emotions'
 sql_user = 'root'
